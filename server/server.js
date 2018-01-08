912b43c97db383da76362b4cf225611ece54486b
Elliott Jones <elliottohj44@gmail.com>
2018-01-07 23:04:51 -0500

    Add updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -19,18 +21,14 @@ io.on('connection', (socket) => {
   
   socket.on('join', (params, callback) => {
     if(!isRealString(params.name) || !isRealString(params.room)) {
-      callback('Name and room name are required.');
+      return callback('Name and room name are required.');
     }
     
     socket.join(params.room);
-    // socket.leave(params.room); <-- For when a user leaves a room
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
     
-    // io.emit <-- sends the message to every single connected user
-       // To target users in specific rooms, e.g. --> io.to('The Office fans').emit 
-    // socket.broadcast.emit <-- sends message to everyone connected to socket server except for the current user
-       // To target users in specific rooms, e.g. --> socket.broadcast.to('The Office fans').emit
-    // socket.emit <-- emits an event to only one specific user
-    
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
     callback();
@@ -39,7 +37,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
   });
   
   socket.on('createLocationMessage', (coords) => {
@@ -47,10 +45,22 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    var user = users.removeUser(socket.id);
+    
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
   });
 });
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
+
+// socket.leave(params.room); <-- For when a user leaves a room
+// io.emit <-- sends the message to every single connected user
+   // To target users in specific rooms, e.g. --> io.to('The Office fans').emit 
+// socket.broadcast.emit <-- sends message to everyone connected to socket server except for the current user
+   // To target users in specific rooms, e.g. --> socket.broadcast.to('The Office fans').emit
+// socket.emit <-- emits an event to only one specific user
