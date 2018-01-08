46bbf16789db6f8cc9787067fccba9eb28220f5d
Elliott Jones <elliottohj44@gmail.com>
2018-01-08 13:43:44 -0500

    Final tweeks

---
+++ playground/new-features.js
@@ -0,0 +1,10 @@
+// 1. Make usernames unique
+// 2. Case INsensitive chat room names, 
+	// e.g. users who try to join either the "LOTR"
+	// room or the "lotr" room would both end up in 
+	// the same room.  
+// 3. Add list of currently active chat rooms, in a 
+// select-dropdown, could populate the same way the 
+// people's list populates. Then instead of typing in 
+// a name, one could just select the chat room they
+// want to join.   
--- server/utils/users.js
+++ server/utils/users.js
@@ -15,6 +15,7 @@ class Users {
 	}
 	addUser (id, name, room) {
 		var user = {id, name, room};
+		
 		this.users.push(user);
 		return user;
 	}
